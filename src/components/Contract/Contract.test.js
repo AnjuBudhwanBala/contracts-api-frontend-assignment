32c741873364dfbbdb23d10f9b3a21d6b0ac4da7
AnjuBala <anjubudhwan@gmail.com>
2019-09-18 17:41:16 +0200

    all changes done

--- src/components/Contract/Contract.test.js
+++ src/components/Contract/Contract.test.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Contract from './Contract';
-import { renderHook, act } from '@testing-library/react-hooks';
-import CustomButton from '../CustomButton/CustomButton';
 
 describe('Contract Component', () => {
   let wrapper;
   let mockRefreshContracts;
+  let mockContracts = [
+    { contractId: 'contractId01' },
+    { contractId: 'contractId02' }
+  ];
 
   beforeEach(() => {
     mockRefreshContracts = jest.fn();
 
     const mockProps = {
-      contractInfo: {
-        company: 'abc'
-      },
+      contractInfo: mockContracts,
       refreshContracts: mockRefreshContracts
     };
 
--- src/pages/Contracts/Contracts.test.js
+++ src/pages/Contracts/Contracts.test.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Spinner from '../../components/Spinner/Spinner';
-import Contract from '../../components/Contract/Contract';
+
 import { Contracts } from './Contracts';
 
 describe('Contracts Component', () => {
-  let wrapper;
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  let wrapper = shallow(<Contracts />);
 
   it('should render Contracts component', () => {
     expect(wrapper).toMatchSnapshot();
